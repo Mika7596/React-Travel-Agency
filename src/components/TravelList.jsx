f6d6e525858c7ca467ff6ff25c22812de5f7948d
Mika7596 <mika7596@gmail.com>
2025-09-07 19:13:11 +0200

    Added labels to travel cards depending on their total cost

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,22 +1,38 @@
-import data from '../assets/travel-plans.json'
+import data from "../assets/travel-plans.json";
 function TravelList() {
-    
   return (
     <div>
-        {data.map(item =>{
-            return (
-                <div key={item.id} className='travel-container'>
-                    <div className='img-container'><img src={item.image} alt={item.destination} /></div>
-                    <div>
-                        <h3>{item.destination} ({item.days} Days)</h3>
-                        <p><i>{item.description}</i></p>
-                        <h4>Price: {item.totalCost}$</h4>
-                    </div>
-                </div>
-            )
-        })}
+      {data.map((item) => {
+        return (
+          <div key={item.id} className="travel-container">
+            <div className="img-container">
+              <img src={item.image} alt={item.destination} />
+            </div>
+            <div>
+              <h3>
+                {item.destination} ({item.days} Days)
+              </h3>
+              <p>
+                <i>{item.description}</i>
+              </p>
+              <h4>Price: {item.totalCost}$</h4>
+              <div className="labels">
+                {item.totalCost <= 350 && (
+                  <div className="deal">Great Deal</div>
+                )}
+                {item.totalCost >= 1500 && (
+                  <div className="premium">Premium</div>
+                )}
+                {item.allInclusive && (
+                  <div className="all-inclusive">All Inclusive</div>
+                )}
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default TravelList
+export default TravelList;
